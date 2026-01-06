25cd5b1b69f8b5649f43b6aa314e304710e72467
koochanghyen <kuku0746@naver.com>
2026-01-06 22:53:55 +0900

    first commit

---
+++ src/main/java/org/example/practicegitaction/PracticeGitActionApplication.java
@@ -0,0 +1,13 @@
+package org.example.practicegitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGitActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeGitActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/practicegitaction/PracticeGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practicegitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGitActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
