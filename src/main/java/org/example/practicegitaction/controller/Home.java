da310cbd2c7ee1ba26c05dde19988f5dcb32938f
koochanghyen <kuku0746@naver.com>
2026-01-11 18:03:08 +0900

    feat:homepage 수정

--- src/main/java/org/example/practicegitaction/controller/Home.java
+++ src/main/java/org/example/practicegitaction/controller/Home.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
+@RequestMapping("/home")
 @RestController
 public class Home {
-    @GetMapping
+    @GetMapping("/get")
     public String home(){
         return "home page";
     }
